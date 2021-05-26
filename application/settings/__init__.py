2b13860d692cec42ca11dfe20ef4e9de3aab54db
Alexsander Uglin <uglin76@mail.ru>
2021-05-26 21:01:25 +0300

    feat: добавлена загрузка переменной окружения BOT_TOKEN

---
+++ application/settings/__init__.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+def load_env_values():
+    """ загружает переменные окружения из .env файла """
+    env_file_path = Path.cwd() / 'application' / 'settings' / '.env'
+    return load_dotenv(env_file_path) if env_file_path.exists() else False
+
+
+if load_env_values():
+    BOT_TOKEN = os.getenv('bot_token')
+
+
+
