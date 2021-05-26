15fc872a89c2ea32830d0f3cedb5e873e4dceda2
Alexsander Uglin <uglin76@mail.ru>
2021-05-27 00:39:59 +0300

    refactor: обновлено приветственное сообщение бота

--- application/classes/dispatcher.py
+++ application/classes/dispatcher.py
@@ -2,7 +2,11 @@ class Dispatcher:
     @staticmethod
     def process_message(received_message, sender_name):
         if received_message == 'начать':
-            message = f'Здравствуйте, {sender_name}'
+            message = f'Здравствуйте, {sender_name}.\n\n' \
+                      f'Отправте мне id или screen_name\n' \
+                      f'пользователя VK, введеный через #.\n' \
+                      f'Например: #durov.\n\n' \
+                      f'И я постараюсь найти ему подходящую пару.'
             return dict(message=message)
 
         else:
