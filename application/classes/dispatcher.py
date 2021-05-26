2a1bd8d768e4dd5a139e47e7d5b110f608a3f9c2
Alexsander Uglin <uglin76@mail.ru>
2021-05-27 00:23:28 +0300

    feat: добавлено новое правило для Dispatcher.process_message()
    для всех неизвестных команд возвращается 'Неизвестная команда'

--- application/classes/dispatcher.py
+++ application/classes/dispatcher.py
@@ -1,6 +1,10 @@
 class Dispatcher:
     @staticmethod
-    def process_message(received_message, sender_name, message=None):
+    def process_message(received_message, sender_name):
         if received_message == 'начать':
             message = f'Здравствуйте, {sender_name}'
-        return dict(message=message)
+            return dict(message=message)
+
+        else:
+            message = 'Неизвестная команда'
+            return dict(message=message)
