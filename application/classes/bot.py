d138968fc3e1b86a168034677a72636814aa44a8
Alexsander Uglin <uglin76@mail.ru>
2021-05-27 14:10:34 +0300

    feat: добавил классу Bot метод _check_user_error_deactivate

--- application/classes/bot.py
+++ application/classes/bot.py
@@ -6,7 +6,7 @@ from ..utilites.logger import set_logger
 from ..classes.dispatcher import Dispatcher
 from ..classes.user import User
 
-from application.settings import BOT_TOKEN
+from application.settings import BOT_TOKEN, API_VERSION
 
 logger = set_logger(__name__)
 
@@ -32,24 +32,28 @@ class Bot(BotAuht):
                 logger.info(f"{sender_name}: {received_message}")
 
                 if '#' in received_message:
-                    self._process_search(received_message[1:], sender_id)
+                    search_user_id = received_message[1:]
+                    user = User(search_user_id)
+                    result, result_message = self._check_user_error_deactivated(user)
+
+                    if result:
+                        self._send_message(sender_id, message=user)
+                    else:
+                        self._send_message(sender_id, **result_message)
+
                 else:
                     message = dispatcher.process_message(received_message, sender_name)
                     self._send_message(sender_id, **message)
 
-    def _process_search(self, user_id, sender_id):
-        """ обработка поискового запроса """
-        user = User(user_id)
-
+    @staticmethod
+    def _check_user_error_deactivated(user):
         if user.has_error:
-            message = dict(message=user.has_error)
+            return False, dict(message=user.has_error)
         elif user.is_deactivated:
-            message = dict(message=user.is_deactivated)
+            return False, dict(message=user.is_deactivated)
         else:
-            message = dict(message=user)
+            return True, None
 
-        self._send_message(sender_id, **message)
-                
     def _send_message(self, sender_id, message):
         """ посылает сообщение пользователю """
         self.api.messages.send(peer_id=sender_id, message=message, random_id=get_random_id())
