c3322728704058a2e4fc0098374a9967a4347aa7
Alexsander Uglin <uglin76@mail.ru>
2021-05-31 09:14:03 +0300

    refactor: перевел все клавиатуры в inline режим

--- application/classes/keyboards.py
+++ application/classes/keyboards.py
@@ -3,27 +3,27 @@ from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 class Keyboards:
     @staticmethod
     def search():
-        keyboard = VkKeyboard(one_time=True)
+        keyboard = VkKeyboard(one_time=False, inline=True)
         keyboard.add_button('Поиск', color=VkKeyboardColor.PRIMARY)
         return keyboard.get_keyboard()
 
     @staticmethod
     def main():
-        keyboard = VkKeyboard(one_time=True)
+        keyboard = VkKeyboard(one_time=False, inline=True)
         keyboard.add_button('Поиск', color=VkKeyboardColor.PRIMARY)
         keyboard.add_button('Инфо', color=VkKeyboardColor.NEGATIVE)
         return keyboard.get_keyboard()
 
     @staticmethod
     def choose_search_option_by_age():
-        keyboard = VkKeyboard(one_time=True)
+        keyboard = VkKeyboard(one_time=False, inline=True)
         keyboard.add_button('Ровестники', color=VkKeyboardColor.PRIMARY)
         keyboard.add_button('Диапазон', color=VkKeyboardColor.NEGATIVE)
         return keyboard.get_keyboard()
 
     @staticmethod
     def choose_search_option_by_sex():
-        keyboard = VkKeyboard(one_time=True)
+        keyboard = VkKeyboard(one_time=False, inline=True)
         keyboard.add_button('Мужчины', color=VkKeyboardColor.PRIMARY)
         keyboard.add_button('Женщины', color=VkKeyboardColor.POSITIVE)
         keyboard.add_button('Неважно', color=VkKeyboardColor.NEGATIVE)
