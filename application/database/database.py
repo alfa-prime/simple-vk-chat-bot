5c1413695c4622c3f51549182e0b0f494e4ba873
Alexsander Uglin <uglin76@mail.ru>
2021-06-13 15:48:35 +0300

    refactor: небольшой рефакторинг

--- application/database/database.py
+++ application/database/database.py
@@ -9,34 +9,39 @@ logger = set_logger(__name__)
 
 try:
     engine = create_engine(DB_CONNECTION)
+
+    Base = declarative_base()
+
+
+    class Users(Base):
+        __tablename__ = "users"
+        user_id = Column(Integer, primary_key=True)
+        vk_user_id = Column(Integer)
+        name = Column(String)
+        link = Column(String)
+
+
+    class BlackList(Base):
+        __tablename__ = 'blacklist'
+        black_id = Column(Integer, primary_key=True)
+        target_id = Column(Integer)
+        user_id = Column(Integer, ForeignKey('users.user_id'))
+
+
+    class WhiteList(Base):
+        __tablename__ = 'whitelist'
+        white_id = Column(Integer, primary_key=True)
+        id = Column(Integer)
+        name = Column(String)
+        link = Column(String)
+        bdate = Column(String)
+        user_id = Column(Integer, ForeignKey('users.user_id'))
+
+    Base.metadata.create_all(engine)
+
 except AttributeError as error:
     error_message = 'Не указана строка подключения базы данных'
     logger.error(error_message)
     exit()
 
-Base = declarative_base()
 
-class Users(Base):
-    __tablename__ = "users"
-    user_id = Column(Integer, primary_key=True)
-    vk_user_id = Column(Integer)
-    name = Column(String)
-    link = Column(String)
-
-class BlackList(Base):
-    __tablename__ = 'blacklist'
-    black_id = Column(Integer, primary_key=True)
-    target_id = Column(Integer)
-    user_id = Column(Integer, ForeignKey('users.user_id'))
-
-class WhiteList(Base):
-    __tablename__ = 'whitelist'
-    white_id = Column(Integer, primary_key=True)
-    id = Column(Integer)
-    name = Column(String)
-    link = Column(String)
-    bdate = Column(String)
-    user_id = Column(Integer, ForeignKey('users.user_id'))
-
-
-Base.metadata.create_all(engine)
