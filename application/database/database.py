bbaf415d43bf6ab7972b0b8c648d57d5084e413a
Alexsander Uglin <uglin76@mail.ru>
2021-06-13 15:55:47 +0300

    refactor: небольшой рефакторинг

--- application/database/database.py
+++ application/database/database.py
@@ -9,9 +9,7 @@ logger = set_logger(__name__)
 
 try:
     engine = create_engine(DB_CONNECTION)
-
     Base = declarative_base()
-
 
     class Users(Base):
         __tablename__ = "users"
@@ -20,13 +18,11 @@ try:
         name = Column(String)
         link = Column(String)
 
-
     class BlackList(Base):
         __tablename__ = 'blacklist'
         black_id = Column(Integer, primary_key=True)
         target_id = Column(Integer)
         user_id = Column(Integer, ForeignKey('users.user_id'))
-
 
     class WhiteList(Base):
         __tablename__ = 'whitelist'
@@ -43,5 +39,3 @@ except AttributeError as error:
     error_message = 'Не указана строка подключения базы данных'
     logger.error(error_message)
     exit()
-
-
