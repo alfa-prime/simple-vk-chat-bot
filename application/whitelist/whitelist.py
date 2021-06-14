3017e9617eca244662d37ac5a8b8f25562e232a1
Alexsander Uglin <uglin76@mail.ru>
2021-06-14 10:59:43 +0300

    refactor: небольшой рефакторинг

--- application/whitelist/whitelist.py
+++ application/whitelist/whitelist.py
@@ -11,12 +11,12 @@ class CurrentWhiteList:
         self.total = 0
         self._counter = 0
 
-        self.items = self._get_items(user_id)
+        self.items = iter(self._get_items(user_id))
 
     def _get_items(self, user_id):
         result = self.db_session.query(WhiteList).filter_by(user_id=user_id).all()
         self.total = len(result)
-        return iter(result) if self.total > 0 else False
+        return result if self.total > 0 else False
 
     def __iter__(self):
         return self
